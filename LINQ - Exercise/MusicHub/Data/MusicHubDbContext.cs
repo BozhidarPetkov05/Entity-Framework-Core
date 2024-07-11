5407eb90d5afb3110a866af7bd4743d18978dd50
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-11 19:22:49 +0300

    Database Creation Code Done

--- LINQ - Exercise/MusicHub/Data/MusicHubDbContext.cs
+++ LINQ - Exercise/MusicHub/Data/MusicHubDbContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace MusicHub.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class MusicHubDbContext : DbContext
     {
@@ -24,7 +25,15 @@
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-
+            builder.Entity<SongPerformer>()
+                .HasKey(sp => new { sp.SongId, sp.PerformerId });
         }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongsPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
