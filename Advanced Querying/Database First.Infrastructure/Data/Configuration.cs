820d7a7d18260c21a5b54464562318ff870c81c8
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-12 17:29:43 +0300

    Resources Updated
    Updated the resources

---
+++ Advanced Querying/Database First.Infrastructure/Data/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First.Infrastructure.Data
+{
+    internal static class Configuration
+    {
+        internal const string ConnectionString = 
+            "Data Source=.\\SQLEXPRESS;Database=SoftUni;User Id=DESKTOP-58IIM9O\\XidZ01;Password=;Trusted_Connection=True;";
+    }
+}
---
+++ Advanced Querying/Database First/Program.cs
@@ -0,0 +1,16 @@
+﻿using Database_First.Infrastructure.Data;
+using Database_First.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_First
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using SoftUniDbContext context = new SoftUniDbContext();
+
+            
+        }
+    }
+}
