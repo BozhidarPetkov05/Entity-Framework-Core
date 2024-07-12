9b6678d94b0c4aa6b736add5749b60ed0ad73d33
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-12 19:24:33 +0300

    Created Some Methods

--- Advanced Querying/Database First/Program.cs
+++ Advanced Querying/Database First/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Database_First.Infrastructure.Data;
 using Database_First.Infrastructure.Data.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Database_First
 {
@@ -10,7 +12,59 @@ namespace Database_First
         {
             using SoftUniDbContext context = new SoftUniDbContext();
 
+            //Demo with Int
+            DemoQueryWithInt(context);
+
+            //Demo with String
+            DemoQueryWithString(context);
+
+            //Demo with Interpolated String
+            DemoWithInterpolatedString(context);
+
+            //Executing Stored Procedures
+            ExecutingStoredProcedures(context);
+        }
+
+        static void DemoQueryWithInt(SoftUniDbContext context)
+        {
+            int departmentId = 1;
+            string query = "SELECT * FROM Employees WHERE DepartmentId = " + departmentId;
+            var employees = context.Employees
+                .FromSqlRaw(query)
+                .ToList();
+
+            Console.WriteLine($"Demo with int count: {employees.Count}");
+        }
+
+        static void DemoQueryWithString(SoftUniDbContext context)
+        {
+            string jobTitle = "Marketing Assistant";
+            string query = "SELECT * FROM Employees WHERE JobTitle = {0}";
+            var employees = context.Employees
+                .FromSqlRaw(query, jobTitle)
+                .ToList();
             
+            Console.WriteLine($"Demo with normal string count: {employees.Count}");
+        }
+
+        static void DemoWithInterpolatedString(SoftUniDbContext context)
+        {
+            string jobTitle = "Marketing Assistant";
+            FormattableString query = $"SELECT * FROM Employees WHERE JobTitle = {jobTitle}";
+            var employees = context.Employees
+                .FromSqlInterpolated(query)
+                .ToList();
+            
+            Console.WriteLine($"Demo with interpolated string count: {employees.Count}");
+        }
+
+        static void ExecutingStoredProcedures(SoftUniDbContext context)
+        {
+            SqlParameter percent = new SqlParameter("@percent", 10);
+            string procQuery = "EXEC UpdateSalary @percent";
+            int rowsAffected = context.Database.ExecuteSqlRaw(procQuery, percent);
+
+            Console.WriteLine($"Rows Affected From Stored Procedure: {rowsAffected}");
         }
     }
 }
---
+++ Advanced Querying/UpdateSalary.sql
@@ -0,0 +1,4 @@
+USE SoftUni
+CREATE PROCEDURE UpdateSalary @percent INT
+AS
+UPDATE Employees SET Salary = Salary + (Salary/100*@percent)
