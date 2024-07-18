e37ac0cf59873d371af7dc8c18304fbe2596ae87
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-18 17:50:54 +0300

    Fixed Composite Keys

---
+++ Migrations in EF Core - Exercise/AcademicRecordsApp/AcademicRecordsApp/Migrations/20240718135509_InitialMigration.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcademicRecordsApp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseStudent");
+
+            migrationBuilder.CreateTable(
+                name: "StudentsCourses",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "int", nullable: false),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsCourses", x => new { x.CoursesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsCourses_StudentsId",
+                table: "StudentsCourses",
+                column: "StudentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentsCourses");
+
+            migrationBuilder.CreateTable(
+                name: "StudentsCourses",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "int", nullable: false),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsCourses", x => new { x.CoursesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsCourses_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsCourses_StudentsId",
+                table: "StudentsCourses",
+                column: "StudentsId");
+        }
+    }
+}
--- Migrations in EF Core - Exercise/AcademicRecordsApp/AcademicRecordsApp/Program.cs
+++ Migrations in EF Core - Exercise/AcademicRecordsApp/AcademicRecordsApp/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace AcademicRecordsApp
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AcademicRecordsApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Scaffold-DbContext "Server=DESKTOP-58IIM9O\SQLEXPRESS;Database=AcademicRecordsDB; Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
         }
     }
 }
