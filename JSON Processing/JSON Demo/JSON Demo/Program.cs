c8371435c6bda30c24d777213d89c2f5cefbde8a
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-20 16:24:19 +0300

    Added Few Methods

--- JSON Processing/JSON Demo/JSON Demo/Program.cs
+++ JSON Processing/JSON Demo/JSON Demo/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Xml;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace JSON_Demo
 {
@@ -13,7 +16,12 @@ namespace JSON_Demo
                 FullName = "Petar Petrov",
                 Age = 25,
                 Height = 185,
-                Weigth = 83.7
+                Weigth = 83.7,
+                Address = new Address()
+                {
+                    City = "Sofia",
+                    Street = "P. Volov"
+                }
             };
 
 
@@ -41,6 +49,26 @@ namespace JSON_Demo
 
             //Converting the data from JSON into object - Newtonsoft.Json
             ConvertFromJsonToObjectNewtonsoft(person);
+
+            Console.WriteLine(" ");
+
+            //Deserializing into anonymous objext
+            DeserializingToAnonymousObject(person);
+
+            Console.WriteLine(" ");
+
+            //Parsing Data Using Contract Resolver
+            ParsingUsingContractResolver(person);
+
+            Console.WriteLine(" ");
+
+            //Using Linq To JSON
+            LinqToJson(person);
+
+            Console.WriteLine(" ");
+
+            //Using Xml To JSON
+            XmlToJson();
         }
 
         static void ConvertToJson(Person person, JsonSerializerOptions options)
@@ -71,6 +99,107 @@ namespace JSON_Demo
             Person? person1 = JsonConvert.DeserializeObject<Person>(data);
             Console.WriteLine("Newtonsoft Output:");
             Console.WriteLine($"{person1.FullName} is {person1.Age} years old and is {person1.Height} cm high.");
+        }
+
+        static void DeserializingToAnonymousObject(Person person)
+        {
+            string data = JsonConvert.SerializeObject(person);
+            
+            var template = new
+            {
+                FullName = string.Empty,
+                Age = 0,
+                Height = 0,
+                Weight = 0.0
+            };
+
+            var person1 = JsonConvert.DeserializeAnonymousType(data, template);
+
+            Console.WriteLine("Deserializing Into Anonymous Object:");
+            Console.WriteLine($"{person1.FullName} is {person1.Age} years old and is {person1.Height} cm high.");
+        }
+
+        static void ParsingUsingContractResolver(Person person)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new KebabCaseNamingStrategy()
+            };
+
+            var settings = new JsonSerializerSettings()
+            {
+                ContractResolver = contractResolver
+            };
+
+            var template = new
+            {
+                FullName = string.Empty,
+                Age = 0,
+                Height = 0,
+                Weight = 0.0
+            };
+
+            string data = JsonConvert.SerializeObject(person, settings);
+
+            var person1 = JsonConvert.DeserializeAnonymousType(data, template, settings);
+
+            Console.WriteLine("Parsing Using Contract Resolver:");
+            Console.WriteLine(data);
+            Console.WriteLine($"{person1.FullName} is {person1.Age} years old and is {person1.Height} cm high.");
+        }
+
+        static void LinqToJson(Person person)
+        {
+            DefaultContractResolver contractResolver = new DefaultContractResolver()
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            };
+
+            var settings = new JsonSerializerSettings()
+            {
+                ContractResolver = contractResolver
+            };
+
+            var template = new
+            {
+                FullName = string.Empty,
+                Age = 0,
+                Height = 0,
+                Weight = 0.0
+            };
+
+            string data = JsonConvert.SerializeObject(person, settings);
+
+            JObject person1 = JObject.Parse(data);
+
+            Console.WriteLine("Linq to Json:");
+            Console.WriteLine($"{person1.SelectToken("full_name")} is {person1.SelectToken("age")} years old and is {person1.SelectToken("height")} cm high.");
+        }
+
+        static void XmlToJson()
+        {
+            string xml = @"<?xml version='1.0' standalone='no'?>
+            <root>
+                 <person id='1'>
+                    <name>Alan</name>
+                    <url>www.google.com</url>
+                 </person>
+                 <person id='2'>
+                    <name>Louis</name>
+                    <url>www.yahoo.com</url>
+                 </person>
+            </root>";
+
+            //Creating new xmlDocument
+            XmlDocument xmlDocument = new XmlDocument();
+            
+            //Loading the Existing Xml Into the Document
+            xmlDocument.LoadXml(xml);
+
+            string data = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented);
+
+            Console.WriteLine("Xml to Json:");
+            Console.WriteLine(data);
         }
     }
 }
--- JSON Processing/JSON Demo/JSON Demo/Test.cs
+++ JSON Processing/JSON Demo/JSON Demo/Test.cs
@@ -6,23 +6,20 @@ using System.Threading.Tasks;
 
 namespace JSON_Demo
 {
-    public class Person
+    public class Rootobject
     {
-        public string FullName { get; set; } = null!;
-
+        public string FullName { get; set; }
         public int Age { get; set; }
-
         public int Height { get; set; }
-
-        public double Weigth { get; set; }
-
+        public float Weigth { get; set; }
         public Address Address { get; set; }
     }
 
+    /*
     public class Address
     {
         public string City { get; set; }
-
         public string Street { get; set; }
     }
+    */
 }
