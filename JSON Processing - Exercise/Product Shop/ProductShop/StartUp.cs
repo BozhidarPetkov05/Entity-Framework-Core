fd7da70e0e20dfff4eefccf7ccbc26b5bcea2c9b
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-20 17:26:55 +0300

    First 3 Exercises Done

--- JSON Processing - Exercise/Product Shop/ProductShop/StartUp.cs
+++ JSON Processing - Exercise/Product Shop/ProductShop/StartUp.cs
@@ -1,9 +1,52 @@
-﻿namespace ProductShop
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            ProductShopContext context = new ProductShopContext();
+            string userText = File.ReadAllText("../../../Datasets/categories.json");
+            Console.WriteLine(ImportCategories(context, userText));
+        }
+
+        //P01. Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        //P02. Import Products
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+            
+            return $"Successfully imported {products.Count}";
+        }
+
+        //P03. Import Categories
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(inputJson);
+
+            categories.RemoveAll(c => c.Name == null);
+            context.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
         }
     }
 }
