17a2f9088e04149cd231b6e51a11f6412fb472bf
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-24 11:29:09 +0300

    Added a Method

--- XML Processing - Exercise/Car Dealer/CarDealer/StartUp.cs
+++ XML Processing - Exercise/Car Dealer/CarDealer/StartUp.cs
@@ -1,10 +1,44 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
 
+            //09
+            string suppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(context, suppliersXml));
+        }
+
+        //P09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SupplierImportDTO[]), new XmlRootAttribute("Suppliers"));
+
+            SupplierImportDTO[] importDtos;
+            using (var reader = new StringReader(inputXml))
+            {
+                importDtos = (SupplierImportDTO[])serializer.Deserialize(reader);
+            };
+
+            Supplier[] suppliers = importDtos
+                .Select(s => new Supplier()
+                {
+                    Name = s.Name,
+                    IsImporter = s.IsImporter
+                })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
         }
     }
 }
