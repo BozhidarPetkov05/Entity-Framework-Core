0d5849511627d42804508798ea2fd76f14aaba93
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-25 16:14:54 +0300

    Added Xml Serializer

---
+++ XML Processing - Exercise/XmlSerializer/XmlSerializer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XmlSerializerMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static string SerializeToXml<T>(T dto, string xmlRootAttribute, bool omitDeclaration = false)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttribute));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = omitDeclaration,
+                Encoding = new UTF8Encoding(false),
+                Indent = true
+            };
+
+            using (StringWriter stringWriter = new StringWriter(stringBuilder, CultureInfo.InvariantCulture))
+            using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, settings))
+            {
+                XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+                xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+                try
+                {
+                    xmlSerializer.Serialize(xmlWriter, dto, xmlSerializerNamespaces);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
