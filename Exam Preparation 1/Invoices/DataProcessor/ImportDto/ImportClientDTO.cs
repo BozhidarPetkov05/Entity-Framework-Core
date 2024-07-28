489f77168b780e03825aafc329fb4fcc5c6e7b7f
Bozhidar Petkov <xidz01bog@gmail.com>
2024-07-28 14:22:15 +0300

    Added DTOs

---
+++ Exam Preparation 1/Invoices/DataProcessor/ImportDto/ImportClientDTO.cs
@@ -0,0 +1,20 @@
+﻿using Invoices.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    [XmlType(nameof(Client))]
+    public class ImportClientDTO
+    {
+        [XmlElement(nameof(Name))]
+        public string Name { get; set; }
+
+        [XmlElement(nameof(NumberVat))]
+        public string NumberVat { get; set; }
+    }
+}
